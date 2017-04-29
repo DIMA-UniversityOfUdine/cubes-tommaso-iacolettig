3efde64e71e2ddab968a1a5a01af33787f495d38
Tommaso Iacolettig <tommaso@iacolettig.it>
2017-04-29 11:10:27 +0200

    Tree model added
    Now some trees are shown on the volcano

--- resources/models.js
+++ resources/models.js
@@ -10,11 +10,13 @@ function drawTerrain(resolution, maxRes) {
         var waterOpacity = 0.5;
         var data = getHeightData(img, 0.3);
         var scaleXZ = maxRes / resolution; // Scale factor according to the resolution
+        var treeProbability = 1/20 * scaleXZ;
+        var treeScale = 0.1;
 
         var terrain = new THREE.Group();
 
         var geometry = new THREE.BoxGeometry(size, size, size); // Same geometry for ground and water
-        var waterMaterial = new THREE.MeshPhongMaterial( {color: 0x3498db, transparent: true, opacity: waterOpacity} );
+        var waterMaterial = new THREE.MeshPhongMaterial( {color: col("blue", 0), transparent: true, opacity: waterOpacity} );
 
         for(var i = 0; i < data.length; i++) {
             var height = data[i];
@@ -51,6 +53,16 @@ function drawTerrain(resolution, maxRes) {
                 water.position.set(i % resolution * size, height * size + waterScale * size / 2, i / resolution * size); // Water positioning (similar to the previous one, the only difference is in the y value where I have to consider the water size as well)
                 terrain.add(water);
             }
+
+            // Trees
+            if(c == col("green", 0) || c == col("green", 1)) { // Only if the ground color is green
+                if(Math.random() < treeProbability) {
+                    var tree = drawTree();
+                    tree.scale.set(treeScale / scaleXZ, treeScale, treeScale / scaleXZ); // Tree XZ-scaling is necessary to counteract the terrain one
+                    tree.position.set(i % resolution * size, height * size, i / resolution * size);
+                    terrain.add(tree);
+                }
+            }
         }
 
         terrain.position.set(-resolution * size / 2, 0, -resolution * size / 2); // Terrain centering
@@ -59,4 +71,29 @@ function drawTerrain(resolution, maxRes) {
     }
 
     return terrain_scaleContainer;
+}
+
+function drawTree() {
+    // Trunk
+    var trunkGeometry = new THREE.BoxGeometry(0.2, 1, 0.2);
+    var trunkMaterial = new THREE.MeshPhongMaterial( {color: col("brown", 1)} );
+    var trunk = new THREE.Mesh(trunkGeometry, trunkMaterial);
+    trunk.castShadow = true;
+    trunk.receiveShadow = true;
+    trunk.position.set(0, trunkGeometry.parameters.height / 2, 0);
+
+    // Foliage
+    var foliageGeometry = new THREE.BoxGeometry(1, 1, 1);
+    var foliageMaterial = new THREE.MeshPhongMaterial( {color: col("green", 2)} );
+    var foliage = new THREE.Mesh(foliageGeometry, foliageMaterial);
+    foliage.castShadow = true;
+    foliage.receiveShadow = true;
+    foliage.position.set(0, trunkGeometry.parameters.height + foliageGeometry.parameters.height / 2, 0);
+
+    // Tree
+    var tree = new THREE.Group();
+    tree.add(trunk);
+    tree.add(foliage);
+
+    return tree;
 }
