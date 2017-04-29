5a83754175668520d08f7cadb9b39a902c014f1c
Tommaso Iacolettig <tommaso@iacolettig.it>
2017-04-29 10:48:51 +0200

    New colors added
    Green and yellow

--- resources/colors.js
+++ resources/colors.js
@@ -1,8 +1,10 @@
 function col(name, index) {
 	var colors = {
-        "brown": [0x201010],
-        "green": [0x1e770e, 0x144f09],
-        "grey": [0xc1c1b0, 0x262121]
+        "blue": [0x3498db],
+        "brown": [0x201010, 0x632f1f],
+        "green": [0x1e770e, 0x144f09, 0x33b51c],
+        "grey": [0xc1c1b0, 0x262121],
+        "yellow": [0xf39c12]
     }
    
     return colors[name][index];
